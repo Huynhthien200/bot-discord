4a11f6f22a0e77ae8745adaa652cf24eeaf8602b
Huynhthien200 <128266121+Huynhthien200@users.noreply.github.com>
2025-06-14 00:08:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,31 @@
 import sys, types
 sys.modules['audioop'] = types.ModuleType('audioop')
 
+print("[sitecustomize] LOADED from", __file__)
+import sys, types
+sys.modules['sui.ml'] = types.ModuleType('sui.ml')   # chặn import ml
+
+class Dummy(types.ModuleType):
+    def __getattr__(self, name):
+        full = f"{self.__name__}.{name}"
+        if full in sys.modules:
+            return sys.modules[full]
+        sub = Dummy(full)
+        sub.__path__ = []          # đánh dấu là package
+        sys.modules[full] = sub
+        return sub
+    def __call__(self, *a, **kw): return self
+    def __iter__(self): return iter(())
+    def __len__(self): return 0
+    def __bool__(self): return False
+
+for pkg in ("numpy", "pandas", "sklearn"):
+    root = Dummy(pkg)
+    root.__path__ = []
+    sys.modules[pkg] = root
+
+print("[sitecustomize] Dummy ML stubs loaded ✔")
+
 import os, requests, discord, asyncio
 from discord.ext import commands, tasks
 from sui_py import SuiAccount, SyncClient, sui_txn         # pip install sui-py
